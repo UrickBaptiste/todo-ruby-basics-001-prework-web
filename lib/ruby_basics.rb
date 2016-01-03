b4d54aece95037e41d7104342e6d66b88a441657
UrickBaptiste <urick_baptiste@hotmail.com>
2016-01-03 13:01:20 +0000

    nitrous-backup at Sun Jan  3 13:01:20 UTC 2016

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,33 @@
-def division(num1, num2)
- 
+def division(num1,num2)
+  return num1/num2
+end
+division(num1,num2)
+
+def assign_variable(value = "bob")
+  puts "value"
+end
+assign_variable
+
+def argue(phrase)
+  puts "I'm right and you are wrong!"
 end
 
-def assign_variable(value)
-  
+def greeting(greeting = "Hi there", name = "Bobby")
+   greeting(greeting, name)
+   greeting(greeting)
 end
 
-def argue
-  
-end
-
-def greeting
-
-end
-
-def return_a_value
-
+def return_a_value(phrase)
+  phrase = "Nice"
+  return phrase
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(food = "cheese")
+  return food
 end
+
+pizza_party
